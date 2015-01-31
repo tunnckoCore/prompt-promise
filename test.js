374647b0b6f728d7a35d1dba90e71112ef6780dc
tunnckoCore <mameto_100@mail.bg>
2015-01-31 20:10:42 +0200

    add test.js

---
+++ test.js
@@ -0,0 +1,14 @@
+/**
+ * prompt-promise <https://github.com/tunnckoCore/prompt-promise>
+ *
+ * Copyright (c) 2014-2015 Charlike Mike Reagent, contributors.
+ * Released under the MIT license.
+ */
+
+'use strict';
+
+var promptPromise = require('./index');
+
+describe('prompt-promise', function() {
+  // body
+});
